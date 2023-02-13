dbb8fd591b96e7bd02c204a609d4e454a2328dfd
Victor Jose Arana Rodriguez <victorj.arana@gmail.com>
2023-02-13 00:20:56 -0600

    :webcam: capture, image from webcam

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Observable, Subject } from 'rxjs';
+import { WebcamImage, WebcamInitError, WebcamUtil } from 'ngx-webcam';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
+  private trigger: Subject<any> = new Subject();
+  private webcamImage!: WebcamImage;
+  private nextWebCam: Subject<any> = new Subject();
+  sysImage = '';
+  ngOnInit() {}
+  public getSnapshot(): void {
+    this.trigger.next(void 0);
+  }
+  public captureImg(webcamImage: WebcamImage): void {
+    this.webcamImage = webcamImage;
+    this.sysImage = webcamImage!.imageAsDataUrl;
+    console.info('got webcam image', this.sysImage);
+  }
+  public get invokeObservable(): Observable<any> {
+    return this.trigger.asObservable();
+  }
+  public get nextWebCamObservable(): Observable<any> {
+    return this.nextWebCam.asObservable();
+  }
   title = 'Tour of Heroes';
 }
